9f0ccd577c09aef56c1ff965e5f99401e58455a8
fanman <ejfamanas@gmail.com>
2025-05-24 13:38:45 +0100

    full blat

--- src/helpers/models/typings.d.ts
+++
@@ -1,9 +0,0 @@
-import {ReferenceType} from "../type-helpers/reference-type-helper";
-import {Identifier, KeywordTypeNode} from "typescript";
-
-export interface ITypedIdentifier {
-    readonly identifier: Identifier;
-    readonly referenceType?: ReferenceType;
-    readonly keyword?: KeywordTypeNode;
-}
-
--- src/hoc-codegen/classes/http-class-codegen.ts
+++
@@ -1,7 +0,0 @@
-import {ClassDeclaration} from "typescript";
-
-export class HttpClassCodegen {
-    public static GenerateFetchAPIService() {
-        // create import statements
-    }
-}
