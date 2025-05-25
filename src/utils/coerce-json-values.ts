d9b84ac30b2e0145a5c84236b43a15997ae376e5
fanman <ejfamanas@gmail.com>
2025-05-25 20:09:01 +0100

    adds logger to coerce json files

--- src/utils/coerce-json-values.ts
+++ src/utils/coerce-json-values.ts
@@ -1,3 +1,5 @@
+import {Logger} from "./logger";
+
 type JSONValue = string | number | boolean | null | JSONObject | JSONArray;
 interface JSONObject { [key: string]: JSONValue; }
 interface JSONArray extends Array<JSONValue> {}
@@ -6,6 +8,8 @@ interface JSONArray extends Array<JSONValue> {}
  * Coerces values like "true" => true, "42" => 42, "null" => null
  */
 export function coerceJsonValue(value: JSONValue): JSONValue {
+    const funcName = 'coerceJsonValue';
+    Logger.debug(funcName, 'Coerce JSON value has started...', value);
     if (typeof value !== 'string') return value;
 
     const trimmed = value.trim().toLowerCase();
@@ -16,12 +20,15 @@ export function coerceJsonValue(value: JSONValue): JSONValue {
 
     const num = Number(value);
     if (!isNaN(num) && value !== '') return num;
-
+    Logger.info(funcName, 'Coerced JSON values...');
     return value;
 }
 
 export function coerceJson(input: JSONValue): JSONValue {
+    const funcName = 'coerceJson';
+    Logger.debug(funcName, 'Coerce Json has started...', input)
     if (Array.isArray(input)) {
+        Logger.info(funcName, 'Returning Json Array')
         return input.map(coerceJson) as JSONArray;
     }
 
@@ -30,9 +37,10 @@ export function coerceJson(input: JSONValue): JSONValue {
         for (const [key, value] of Object.entries(input)) {
             result[key] = coerceJson(value);
         }
+        Logger.info(funcName, 'Returning Json object')
         return result;
     }
-
+    Logger.info(funcName, 'Coercing Json values...')
     return coerceJsonValue(input);
 }
 
